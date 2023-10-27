8f9ad3647cf7e8139cddec7b8b590dd112b14f6d
y_sou <ysoultane@gmail.com>
2023-10-27 11:22:14 +0200

    add test

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-const express = require("express"); const app = express(); app.get("/", (req, res) => { res.send("Express on Vercel"); }); const PORT = process.env.PORT || 5000; app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+const express = require("express"); 
+const app = express(); 
+app.get("/", (req, res) => { 
+    res.send("Express on Vercel"); 
+}); 
+app.get("/test", (req, res) => { 
+    res.send("ceci est un test"); 
+}); 
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 module.exports = app;
